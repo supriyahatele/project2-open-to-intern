ed42dc99a10477f90740e33552ebe1f22a0db3ee
shubhra92 <shubhrogh765@gmail.com>
2022-06-29 11:11:31 +0530

    collegeController

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -9,6 +9,7 @@ const isvalid=function(value){
   return true
 
 }
+let linkCheck=/(https?:\/\/.*\.(?:jpg|jpeg|png|gif))/i
 
 // ===============================[createCollage]=========================================
 const createCollage  = async function (req, res) {
@@ -17,11 +18,17 @@ const createCollage  = async function (req, res) {
     const {name,fullName,logoLink}=data
     
     if(!isvalid(name))return res.status(400).send({status:false, msg:"name is required"})
-    // if(!/^/([0-9])w+.test())
     if(!isvalid(fullName)) return res.status(400).send({status:false, msg:"fullName is required"})
     if(!isvalid(logoLink)) return res.status(400).send({status:false, msg:"logoLink is required"})
     if(!isUri(logoLink)) return res.status(400).send({status:false, msg:"logoLink invalid"})
- 
+    
+
+    if(!(linkCheck.test(logoLink))) return res.status(400).send({status:false, msg:"logoLink invalid"})
+
+    //name unique test
+    const dataCheck=await collagemodel.findOne({name:name})
+    if(dataCheck)return res.status(400).send({status:false, msg:"name is already exist"})
+
     const savedate=await collagemodel.create(data)
      return res.status(201).send({status:false, msg:" college successfully created",data:savedate })
 
@@ -43,4 +50,3 @@ const createCollage  = async function (req, res) {
 
 
 module.exports = {createCollage,getCollageDetail}
-// module.exports.getCollageDetail= getCollageDetail
